aeb8cd457899e9568ec3a88c6c881b90087b2705
alcm-b <egor.berdnikov@gmail.com>
2013-11-12 14:54:52 +0700

    + aux makefile

---
+++ make/generic.mk
@@ -0,0 +1,21 @@
+DD:=$(shell date +%Y-%m-%d_%H%M)
+
+##
+## Display help on Makefile targets
+usage:
+	sed -ne '1 s/.*/Makefile/p; s/^include[ -]\+\(.*Makefile\|.*.mk\)/\1/p' ./Makefile | xargs ls |\
+	while read b; do \
+		a=`readlink -f $$b` ;\
+		printf "\n\nSource $$a:\n" ;\
+		sed -ne 's/^##[[:space:]]*/\t/ p; /^[[:alnum:]-]\+:/ s/^/\t\t/p;' $$a;\
+	done | less
+##
+## Move remaining vim swap files off the way (to a temporary diretory)
+vimclean:
+	mkdir ./-/$(DD)
+	find -L ./$(src)/ -name \*sw[pon]  -exec echo mv {} ./-/$(DD) \;
+	read yy ; if [ 'y' == $$yy ] ; then find -L ./$(src)/ -name \*sw[pon] -exec echo mv2 {} ./-/${$DD} \; ; fi
+## 
+## Find two most recently changed files with a given pattern; display diff between them.
+diff-recent:
+	ls -rt $(file_pattern) | tail -2 | xargs diff -u | vim - -R
