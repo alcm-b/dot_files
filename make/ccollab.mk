b381058c98c2083b460ea49128e197338e6ee0f3
Egor Berdnikov <eberdnikov@odin.com>
2016-02-24 12:16:04 +0600

    Cleaned up CLI ccollaborator review automation scripts

--- make/ccollab.mk
+++ make/ccollab.mk
@@ -1,23 +1,28 @@
 CHANGESET_PATH?=var/ccollab
 CHANGESET = $(CHANGESET_PATH)/current/changeset.list 
-# Collaborator
+SVNDIR = $(shell readlink -f $(CCOLLAB_SVNBRANCH) )
+## 
+## Prepare a changeset for a new review
 ccollab-changeset: up
 	CHANGESETDIR=$(CHANGESET_PATH)/$(DD) \
 		&& mkdir -p $$CHANGESETDIR \
-		&& ( cd $(SVNHOME) && svn -x -w diff ) > $$CHANGESETDIR/changeset.patch \
-		&& ( cd $(SVNHOME) && svn st ) | sed -ne "s/^[AMD]\([[:space:]]\++\)\?//p" > $$CHANGESETDIR/changeset.list \
+		&& ( cd $(SVNDIR) && svn -x -w diff ) > $$CHANGESETDIR/changeset.patch \
+		&& ( cd $(SVNDIR) && svn st ) | sed -ne "s/^[AMD]\([[:space:]]\++\)\?//p" > $$CHANGESETDIR/changeset.list \
 		&& ( cd $(CHANGESET_PATH) && ( [ -s current ] && unlink current) ; ln -s $(DD) current )
 		echo vi -o $(CHANGESET_PATH)/current/*
 
+## 
+## Create a review using previously created changeset
 ccollab-addchanges: cllid=new
 ccollab-addchanges:
 	echo "Changeset:"
 	cat $(CHANGESET)
 	echo
 	FILES=`cat $(CHANGESET) | tr '\n' ' '` \
-	&& (cd $(SVNHOME) && ccollab addchanges $(cllid) $$FILES )
+	&& (cd $(SVNDIR) && ccollab addchanges $(cllid) $$FILES )
+
 ##
-## Take the most recent review file from download dir; unpack the file and create a patch
+## Take the most recent review file from download dir; unpack the file and produce a patch
 ccollab-review: NUM=*
 ccollab-review: DOWNLOAD_PATH=/media/sf_Downloads
 ccollab-review: CLL_REVIEW_HOME=./tmp/ccollab
@@ -27,23 +32,19 @@ ccollab-review: up
 	CLLFILE=`basename "$$CLLZIP"`  ; \
 	CURRENT_REVIEW="$(CLL_REVIEW_HOME)/$${CLLFILE}_$(DD)"; \
 	unzip "$$CLLZIP" -d "$$CURRENT_REVIEW" ; \
-	cd "$$CURRENT_REVIEW" && find . -type f -exec diff -uNw `dirname $(SVNHOME)`/{} {} \; > "../$$CLLFILE.patch" ; \
+	cd "$$CURRENT_REVIEW" && find . -type f -exec diff -uNw $(SVNHOME)/{} {} \; > "../$$CLLFILE.patch" ; \
 	mv "../$$CLLFILE.patch" ./review.patch ; \
 	cd .. && ( [ -s current ] && unlink current) ;\
 	ln -s $${CLLFILE}_$(DD) current ;\
 	echo "Open:\n(cd \" $(CLL_REVIEW_HOME)/current\" && vim \"review.patch\")"
+	cp ./.vimrc $(CLL_REVIEW_HOME)/current 
 
-ccollab-addchanges2: cllid=new
-ccollab-addchanges2:
-	CHANGESETDIR=$(CHANGESET_PATH)/$(DD) \
-		&& mkdir -p $$CHANGESETDIR \
-		&& cd /opt/alf/ref/digitalriver/trunk/ \
-		&& screen -d -m -L ccollab addchanges $(cllid) . \
-		&& sleep 9 \
-		&& CHANGESETFILE=`grep '/tmp/edit-list' screenlog.0 | fromdos | tail -1` \
-		&& vim $$CHANGESETFILE && cp $$CHANGESETFILE $$CHANGESETDIR ; screen -D -RR \
-		&& mv screenlog.* $$CHANGESETDIR/ 
-	# ehm, and now... screen?
-		#&& ccollab addchanges $(cllid) hsbuild/ > $$CHANGESETDIR/ccollab.out & 
+##
+## Commit the approved changeset to SVN
+ccollab-commit: up
 ccollab-commit:
-	cat $(CHANGESET) | ( cd $(SVNHOME) && xargs svn commit -m "$(message)")
+	cat $(CHANGESET)
+	echo 'cd $(SVNDIR) && xargs svn commit -m "$(message)" '
+	echo "'Enter' to continue, Ctrl-C to quit."
+	read a
+	cat $(CHANGESET) | ( cd $(SVNDIR) && xargs svn commit -m "$(message)")
